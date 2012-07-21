62eff80dee8f254efa48f8904f36918db2eb6aa2
Brendan Forster <me@brendanforster.com>
2012-07-21 16:29:22 +1000

    setup testbench

---
+++ spikes/SQLiteTester/SQLiteTester/MainViewModel.cs
@@ -0,0 +1,35 @@
+﻿using GalaSoft.MvvmLight;
+
+namespace SQLiteTester
+{
+    public class MainViewModel : ViewModelBase
+    {
+        string createResult;
+        public string CreateResult
+        {
+            get { return createResult; }
+            set { Set(() => CreateResult, ref createResult, value); }
+        }
+
+        string readResult;
+        public string ReadResult
+        {
+            get { return readResult; }
+            set { Set(() => ReadResult, ref readResult, value); }
+        }
+
+        string updateResult;
+        public string UpdateResult
+        {
+            get { return updateResult; }
+            set { Set(() => UpdateResult, ref updateResult, value); }
+        }
+
+        string deleteResult;
+        public string DeleteResult
+        {
+            get { return deleteResult; }
+            set { Set(() => DeleteResult, ref deleteResult, value); }
+        }
+    }
+}
