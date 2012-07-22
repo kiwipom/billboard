c2364f05a7af43ee42a1585cb2db8d30aa218757
Brendan Forster <me@brendanforster.com>
2012-07-22 21:33:55 +1000

    moved 'welcome' control out to separate element, dismisses now

---
+++ src/Billboard/Modules/Dashboard/WelcomeControl.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Billboard.Modules.Dashboard
+{
+    public sealed partial class WelcomeControl
+    {
+        public WelcomeControl()
+        {
+            this.InitializeComponent();
+        }
+
+        private void DismissWelcome(object sender, RoutedEventArgs e)
+        {
+            var parent = Parent as Panel;
+
+            parent.Children.Remove(this);
+        }
+    }
+}
